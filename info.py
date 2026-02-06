9a3ba676da57c3b85357a3887900c40bf94e6164
ashishpatel8583-ctrl <ashishpatel8583@gmail.com>
2026-02-06 00:49:37 -0800

    Create info.py

---
+++ info.py
@@ -0,0 +1,10 @@
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+OWNER_ID = 123456789
+
+OWNER_FORCE_CHANNEL = -1001234567890   # owner channel/group
+OWNER_FORCE_INVITE = "https://t.me/+xxxx"
+
+SUPPORT_CHANNEL = "https://t.me/yourchannel"
+BOT_REPO = "https://github.com/yourrepo"
+
+WEBHOOK_URL = "https://your-render-url.onrender.com"
